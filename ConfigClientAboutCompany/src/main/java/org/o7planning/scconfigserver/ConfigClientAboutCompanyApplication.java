468f622ab70fc803111bb2e4f43a903f078a6cc7
JP204 <34078171+JP204@users.noreply.github.com>
2020-02-03 01:20:16 -0600

    Add files via upload

---
+++ ConfigClientAboutCompany/src/main/java/org/o7planning/scconfigserver/ConfigClientAboutCompanyApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.scconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientAboutCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientAboutCompanyApplication.class, args);
+	}
+
+}
